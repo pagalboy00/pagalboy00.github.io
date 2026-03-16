aaeca9301a22a19bec61b004c5d0a8f44ffaa729
Radheshyam Janwa <124763735+pagalboy00@users.noreply.github.com>
2026-03-16 10:44:41 +0530

    ADD2

---
+++ assets/assets.js
@@ -0,0 +1,213 @@
+import user_image from './user-image.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import ct from './ct.png';
+// import image from './image.png';
+import AZ_204 from './AZ-204.png';
+import digital from './digital.png';
+import data from './data.png';
+import power from './power.png';
+import bec from './BEC.png';
+import  factory from './factory.png';
+import lake from './lake.png' ;
+import elastic from './elastic-stack.png';
+import docker from './docker.png';
+import nmap from './nmap.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import ceh from './CEH.png'
+import cc from './cc.png';
+import acp from './ACP.png';
+import rhcsa from './rhcsa.png';
+import detect from './cyber-detective.png';
+import Fabric from './Fabric.png';
+import CSA from './CSA.png';
+import microsoft from './microsoft.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import up_police from './up_police.png';
+
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    digital,
+    data,
+    ct,
+    bec,
+    up_police,
+    // image,
+    power,
+    factory,
+    lake,
+    elastic,
+    docker,
+    nmap,
+    vscode,
+    firebase,
+    figma,
+    git,
+    AZ_204,
+    mongodb,
+    right_arrow_white,
+    ceh,
+    cc,
+    acp,
+    rhcsa,
+    detect,
+    Fabric,
+    CSA,
+    microsoft,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark
+};
+
+export const workData = [
+    {
+        title: 'Image Analyser',
+        description: 'Forensic',
+        bgImage: '/image.png',
+        link: 'https://github.com/Radheshyam00/ImageAnalyzer00',
+    },
+    {
+        title: 'Log Analyser',
+        description: 'SOC Analyst',
+        bgImage: '/Log.png',
+        link: 'https://github.com/Radheshyam00/Log-Analyser',
+    },
+    {
+        title: 'Call Handler',
+        description: 'Incoming Call Filter System',
+        bgImage: '/Call.png',
+        link: 'https://github.com/Radheshyam00/Call-Handler-00',
+    },
+    {
+        title: 'cPanel/WHM with Proxmox',
+        description: 'System Administrator',
+        bgImage: '/Cpanel.png',
+        link: 'https://github.com/Radheshyam00/Setup-cPanel-on-Azure-with-Proxmox',
+    },
+    {
+        title: 'Secure Hosting on Cloud',
+        description: 'Cloud',
+        bgImage: '/Cloud-Security.png',
+        link: 'https://github.com/Radheshyam00/Secure-Hosting-of-Web-App-on-Azure-Cloud',
+    },
+]
+
+export const serviceData = [
+    { icon: assets.web_icon, title: 'Ethical Hacking', description: 'Ethical hacking is the proactive, authorized practice of detecting vulnerabilities in an application, system, or organization’s infrastructure...', link: 'https://www.simplilearn.com/tutorials/cyber-security-tutorial/what-is-ethical-hacking' },
+    { icon: assets.digital, title: 'DFIR', description: 'DFIR is the process of collecting digital forensic evidence, hunting for suspicious activities, and continuously monitoring for endpoint events...', link: 'https://www.rapid7.com/fundamentals/digital-forensics-and-incident-response-dfir/' },
+    { icon: assets.mobile_icon, title: 'SOC Aanlyst', description: 'An SOC analyst is a key player in cybersecurity, working as a member of a dedicated Security Operations Center (SOC)...', link: 'https://www.offsec.com/cybersecurity-roles/soc-analyst/' },
+    { icon: assets.graphics_icon, title: 'System Administrator', description: 'An IT administrator, system administrator, sysadmin, or admin is a person who is responsible for the upkeep, configuration, and reliable operation of computer systems....', link: 'https://en.wikipedia.org/wiki/System_administrator' },
+    { icon: assets.ui_icon, title: 'Fabric Analyst', description: 'Microsoft Fabric encompasses data science, real-time analytics, storage, and data migration within a unified solution...', link: 'https://www.latentview.com/microsoft-fabric/' },
+    
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Python, Bash Script, HTML, CSS, JavaScript React Js, Next Js' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science and minor in B.B.A' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 4 projects' }
+];
+
+export const toolsData = [
+assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git, assets.docker, assets.nmap,assets.power, assets.data, assets.lake, assets.factory, assets.elastic, assets.bec
+];
+
+export const experienceData = [
+  {
+    role: "Cybersecurity Intern",
+    company: "Uttar Pradesh Police",
+    duration: "June 2025 - June 2025",
+    logo: up_police,
+    points: [
+        "Advanced Network Forensics",
+        "Advanced iOS Forensics",
+        "Dark Web Analysis",
+        "Digital Forensic Investigation",
+        "Cyber Law and Regulations",
+        "Cryptocurrency Analysis and Tracking",
+        "Cyber Threat Intelligence",
+        "Malware Analysis and Reverse Engineering",
+        "Real-World Hacking Techniques",
+        "Monitored and analyzed security alerts in SIEM tools.",
+        "Assisted in threat hunting and vulnerability assessments.",
+        "Documented incident response procedures and reports.",
+    ],
+  },
+  {
+    role: "Cloud Infra and Security Intern",
+    company: "Celebal Technologies",
+    duration: "May 2024 - July 2024",
+    logo: ct,
+    points: [
+        "AWS, Azure, GCP",
+        "VMs, Docker, Kubernetes",
+        "VPC, Subnets, Firewalls",
+        "IAM policies, Load Balancers",
+        "DNS Forwarding, Route traffic",
+        "Application Gateway, Security groups",
+        "VPN, DDoS protection, WAF",
+        "SSL Offloading, CI/CD pipelines"
+    ],
+  },
+  // Add more as needed
+]
+
+
+
